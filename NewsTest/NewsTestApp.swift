a5ff944836e281e1fb5c1ef570414296dc920878
Lukman <lukmanmakhaev@gmail.com>
2023-04-30 16:36:23 +0300

    Initial Commit

---
+++ NewsTest/NewsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTestApp.swift
+//  NewsTest
+//
+//  Created by Lukman Makhaev on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
